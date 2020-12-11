0f2cdfb8b43516437aa584d26b6ccb27a7b10c93
rick2785 <rjhrabowskie@gmail.com>
2020-12-10 18:34:58 -0600

    Initial Commit

---
+++ JuiceUI/JuiceUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  JuiceUIApp.swift
+//  JuiceUI
+//
+//  Created by RJ Hrabowskie on 12/10/20.
+//
+
+import SwiftUI
+
+@main
+struct JuiceUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
